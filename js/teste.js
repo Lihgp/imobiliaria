9044fe36e0aea549a391c4f627f0c5f8e2681039
Cffmatheus <matheusfree16@gmail.com>
2019-09-24 20:41:41 -0300

    a

---
+++ js/teste.js
@@ -0,0 +1,9 @@
+      $('#botaoDivPesquisa').click(function () {
+          debugger
+        $('#divPesquisa').show();
+        $('#divPesquisa2').show();
+      });
+
+      $('.teste').click(function () {
+          console.log("oiiiii");
+      });
