3782b16ecad6fcc15b39d0be029843edea66503b
Lihgp <guimaraesdpa@gmail.com>
2019-09-24 20:37:00 -0300

    alterações

--- jsTeste/main.js
+++ jsTeste/main.js
@@ -15,7 +15,38 @@ $('.click-login').click(function() {
     $('#modal-login').modal("show")
 });
 
-//Função para mudar div
+//Função para o botão Sair
+$('.click-sair').click(function() {
+    //Fazer ir pra página inicial
+    window.location.href = ((window.origin).concat('/html/Imobiliaria.html'));
+});
+
+//Função para remover mensagem de erro do login
+function removeMensagemErro() {
+    $('#alerta-invalido').css('display', 'none')
+}
+
+//Função para validar o login
 $('#enviar-login').click(function () {
-    debugger
+    var usuario = document.getElementById('user').value;
+    var senha = document.getElementById('senha').value;
+
+    if (usuario === 'admin' && senha === 'admin') {
+        removeMensagemErro()
+        $("#barraNavegacaoRestrita").show()
+        $("#barraNavegacaoPublico").hide()
+        $('#modal-login').modal("hide")
+        //Adicionar um efeito de LOAD
+    } else {
+        $("#alerta-invalido").fadeIn(200)
+        $('#alerta-invalido').css('display', 'inline-block')
+        $('#alerta-invalido').css('width', '100%')
+        $('#alerta-invalido').text('Usuário ou senha incorreto.')
+    }
 })
+
+// Função para quando clicar no X do modal, sair a mensagem de erro
+$('.close').click(removeMensagemErro())
+
+// Função para quando sair do modal sair a mensagem de erro
+$("#modal-login").on('hide.bs.modal', removeMensagemErro());
