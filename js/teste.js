e7fc1865f8ef33b9ca273f4f82bfbaa76492f910
Lihgp <guimaraesdpa@gmail.com>
2019-09-24 23:26:55 -0300

    remove

--- js/teste.js
+++
@@ -1,9 +0,0 @@
-      $('#botaoDivPesquisa').click(function () {
-          debugger
-        $('#divPesquisa').show();
-        $('#divPesquisa2').show();
-      });
-
-      $('.teste').click(function () {
-          console.log("oiiiii");
-      });
