d891c30c5c40509a8028a07d0f996132fa8d3a69
Lihgp <guimaraesdpa@gmail.com>
2019-09-24 23:02:29 -0300

    alterações

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ $(window).scroll(function() {
     var scroll = $(window).scrollTop();
 
     if (scroll > 1000) {
-        $('.navbar').addClass('navbar-main');
+        $('.navbar').css('background-color', 'rgb(65, 55, 102)')
     } 
     else {
-        $('.navbar').removeClass('navbar-main');
+        $('.navbar').css('background-color', 'rgba(65, 55, 102, 0.616)')
     }
 });
 
@@ -50,3 +50,31 @@ $('.close').click(removeMensagemErro())
 
 // Função para quando sair do modal sair a mensagem de erro
 $("#modal-login").on('hide.bs.modal', removeMensagemErro());
+
+//Funções para pesquisar - TELA LISTAGEM
+$('.pesquisar-funcionario').click(function (){
+    window.location.href = ((window.origin).concat('/html/Listagem/ListagemFuncionario.html'));
+});
+
+$('.pesquisar-cliente').click(function (){
+    window.location.href = ((window.origin).concat('/html/Listagem/ListagemCliente.html'));
+});
+
+$('.pesquisar-imoveis').click(function (){
+    window.location.href = ((window.origin).concat('/html/Listagem/ListagemImoveis.html'));
+});
+
+$('.pesquisar-interesses').click(function (){
+    window.location.href = ((window.origin).concat('/html/Listagem/ListagemInteresses.html'));
+});
+
+//Função para ir na tela PESQUISAR IMÓVEIS
+$('.buscar-imoveis').click(function (){
+    window.location.href = ((window.origin).concat('/html/Pesquisa.html'));
+});
+
+$('#botaoDivPesquisa').click(function () {
+    debugger
+  $('#divPesquisa').show();
+  $('#divPesquisa2').show();
+});
