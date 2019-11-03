0fcac5787f6493895e15a78d55d79fb9038fee2c
Lihgp <guimaraesdpa@gmail.com>
2019-11-02 22:06:11 -0300

    Front do Cadastro Cliente e Pesquisa

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,76 @@
-// Função para quando mexer o scroll mudar o NAV
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-
-    if (scroll > 1000) {
-        $('.navbar').css('background-color', 'rgb(65, 55, 102)')
-    } 
-    else {
-        $('.navbar').css('background-color', 'rgba(65, 55, 102, 0.616)')
-    }
-});
-
 //Função para o botão Login
-$('.click-login').click(function() {
-    debugger
-    $('#modal-login').modal("show")
+$(".click-login").click(function() {
+  $("#modal-login").modal("show");
 });
 
 //Função para remover mensagem de erro do login
 function removeMensagemErro() {
-    $('#alerta-invalido').css('display', 'none')
+  $("#alerta-invalido").css("display", "none");
 }
 
 //Função para validar o login
-$('#enviar-login').click(function () {
-    debugger
-    var usuario = document.getElementById('user').value;
-    var senha = document.getElementById('senha').value;
+$("#enviar-login").click(function() {
+  var usuario = document.getElementById("user").value;
+  var senha = document.getElementById("senha").value;
 
-    if (usuario === 'admin' && senha === 'admin') {
-        removeMensagemErro()
-        $("#barraNavegacaoPublico").hide()
-        $("#barraNavegacaoRestrita").show()
-        $('#modal-login').modal("hide")
-        //Adicionar um efeito de LOAD
-    } else {
-        $("#alerta-invalido").fadeIn(200)
-        $('#alerta-invalido').css('display', 'inline-block')
-        $('#alerta-invalido').css('width', '100%')
-        $('#alerta-invalido').text('Usuário ou senha incorreto.')
-    }
-})
+  if (usuario === "admin" && senha === "admin") {
+    removeMensagemErro();
+    $("#barraNavegacaoPublico").hide();
+    $("#barraNavegacaoRestrita").show();
+    $("#modal-login").modal("hide");
+    //Adicionar um efeito de LOAD
+  } else {
+    $("#alerta-invalido").fadeIn(200);
+    $("#alerta-invalido").css("display", "inline-block");
+    $("#alerta-invalido").css("width", "100%");
+    $("#alerta-invalido").text("Usuário ou senha incorreto.");
+  }
+});
 
 // Função para quando clicar no X do modal, sair a mensagem de erro
-$('.close').click(removeMensagemErro())
+$(".close").click(removeMensagemErro());
 
 // Função para quando sair do modal sair a mensagem de erro
-$("#modal-login").on('hide.bs.modal', removeMensagemErro());
+$("#modal-login").on("hide.bs.modal", removeMensagemErro());
 
-$('#botaoDivPesquisa').click(function () {
-    debugger
-  $('#divPesquisa').show();
-  $('#divPesquisa2').show();
+$("#botaoDivPesquisa").click(function() {
+  $("#divPesquisa").show();
+  $("#divPesquisa2").show();
 });
+
+// Máscaras para os campos input
+$(document).ready(function() {
+  $("#precoMinimo").mask("#.##0,00", { reverse: true });
+  $("#precoMaximo").mask("#.##0,00", { reverse: true });
+  $("#cep").mask("00000-000");
+  $("#numero").mask("#");
+  $("#cpf").mask("000.000.000-00");
+  $("#celular").mask("#0000-0000", { reverse: true });
+  $("#outroTelefone").mask("#0000-0000", { reverse: true });
+  $("#estado").mask("AA");
+});
+
+// Função de validação dos campos
+(function() {
+  "use strict";
+  window.addEventListener(
+    "load",
+    function() {
+      var forms = document.getElementsByClassName("needs-validation");
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener(
+          "submit",
+          function(event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+          },
+          false
+        );
+      });
+    },
+    false
+  );
+})();
